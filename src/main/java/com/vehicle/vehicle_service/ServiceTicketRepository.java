eec4802570ddb6a414e8af27a9da809489f01604
Mykolas2005 <kociusm@gmail.com>
2026-08-11 19:00:06 +0000

    feat(db): add JPA repositories for User, Vehicle, and ServiceTicket (US7)

--- src/main/java/com/vehicle/vehicle_service/ServiceTicketRepository.java
+++ src/main/java/com/vehicle/vehicle_service/ServiceTicketRepository.java
@@ -3,8 +3,10 @@ package com.vehicle.vehicle_service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface ServiceTicketRepository extends JpaRepository<ServiceTicket, Long> {
 
     List<ServiceTicket> findByVehicleOwner(User owner);
--- src/main/java/com/vehicle/vehicle_service/VehicleRepository.java
+++ src/main/java/com/vehicle/vehicle_service/VehicleRepository.java
@@ -1,9 +1,11 @@
 package com.vehicle.vehicle_service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     List<Vehicle> findByOwner(User owner);
     List<Vehicle> findByOwnerId(Long ownerId);
